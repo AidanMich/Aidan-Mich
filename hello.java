49bfb0669dea38e9f53e8ef64570da60c9873b50
Aidan M <blockprinceUSA@hotmail.com>
2023-08-30 12:11:16 -0700

    added a read file and a write file methods

--- hello.java
+++ hello.java
@@ -1,5 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.*;
+
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    // private StringBuilder str = new StringBuilder ("");
+    public static String read(String fileName) throws IOException {
+        StringBuilder str = new StringBuilder("");
+        BufferedReader ar = new BufferedReader(new FileReader(fileName));
+        while (ar.ready()) {
+            str.append((char) ar.read());
+        }
+        ar.close();
+        return str.toString();
+
     }
+
+    public static void write(String input, String outputFile) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(outputFile);
+        pw.print(input);
+        pw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String file = "readme.txt";
+        System.out.println(read(file));
+
+        String input = "hello there";
+        String outputFile = "output.txt";
+        write(input, outputFile);
+    }
+
 }
